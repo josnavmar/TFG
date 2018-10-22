9da5dfabf90d10f2eafdc928cd5c5b7d6c2cec15
JCRN <JCRN@JCRN-PC>
2018-10-22 21:45:10 +0200

    Django project

---
+++ TwitterStats/secrets.py
@@ -0,0 +1,14 @@
+#this file contains sensitive information like django secret key
+#edit this according to your requirement
+
+class Django_Secrets:
+    def __init__(self):
+        self.key = ''
+
+
+class Oauth_Secrets:
+    def __init__(self):
+        self.consumer_key = "yYdEwtLHYfEoqIgIfXZURTAUl"
+        self.consumer_secret = "vm6ItlkhdAJkqZ7gMJv0oGi97nBFTdYY9Zda2e0TjHFOiJglc1"
+        self.access_token = "967262689-6ZoC0Jxq6wrm8wFZkIepoTxL5dKnyMyM48FCN1L2"
+        self.access_token_secret = "Q8BV0zCtKlfXqpFqLZwy7c5iHWZoXsU3AywMtWKwhysAU"
