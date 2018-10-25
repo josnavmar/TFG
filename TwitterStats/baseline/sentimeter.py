411a6cc0a29880e6cbadc3e38d8fcc8e14754212
JCRN <JCRN@JCRN-PC>
2018-10-25 19:56:58 +0200

    Baseline package add

---
+++ TwitterStats/baseline/sentimeter.py
@@ -0,0 +1,10 @@
+'''
+Created on 22 oct. 2018
+
+@author: JCRN
+'''
+
+def primary(input_hastag):
+    
+    return [['Sentiment', 'no. of tweets'],['Positive',4]
+            ,['Neutral',5],['Negative',6]]
