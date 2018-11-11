f6c8cd00fb9ce80ed2109b54d53ab2e9d350308a
JCRN <JCRN@JCRN-PC>
2018-11-11 20:56:27 +0100

    CNN

--- TwitterStats/apps/cnn/apps.py
+++ TwitterStats/apps/cnn/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class SentimeterConfig(AppConfig):
-    name = 'sentimeter'
+class cnnConfig(AppConfig):
+    name = 'cnn'
--- TwitterStats/apps/cnn/forms.py
+++ TwitterStats/apps/cnn/forms.py
@@ -5,5 +5,3 @@ Created on 22 oct. 2018
 '''
 from django import forms
 
-class userinput(forms.Form):
-    q=forms.CharField(required=True,max_length=15,label='Input #hashtag')
