3d91dc0db6d3eec9f856672ec7b6f5f7382d3cbe
JCRN <JCRN@JCRN-PC>
2019-06-02 12:13:58 +0200

    Pruebas unitarias

--- LearnIA/image/tests.py
+++ LearnIA/image/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import unittest
+from image.funciones import sentgen_function, desgeneraliza, demostracion
 
-# Create your tests here.
+
+class TestFuncionesImagen(unittest.TestCase):
+    
+    
+    def assertNotEmpty(self, obj):
+        self.assertTrue(obj)
+    
+    def test_sentgen(self):
+        self.assertNotEmpty(sentgen_function('Sentimiento', 'Simple CNN', '50', 'SGD'))
+        
+        print('Prueba de introduccion de parametro COMPARA_ALGORITMO a nulo: \n')
+        #sentgen_function('', 'Simple CNN', '50', 'SGD')
+        
+        print('Prueba de introduccion de parametro CSV a nulo: \n')
+        #sentgen_function('Sentimiento', '', '50', 'SGD')
+        
+        print('Prueba de introduccion de parametro TOKENIZE a nulo: \n')
+        #sentgen_function('Sentimiento', 'Simple CNN', '', 'SGD')
+        
+        print('Prueba de introduccion de dos parametro a nulo: \n')
+        #sentgen_function('Genero', '', '', 'SGD')
+    
+    def test_desgeneraliza(self):
+        self.assertEqual(desgeneraliza('Enfadado'), 'Enfado')
+        
+
+if __name__ == "__main__":
+    unittest.main()
--- LearnIA/text/tests.py
+++ LearnIA/text/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import unittest
+from text.funciones import comparacion_algoritmos , tokenize
 
-# Create your tests here.
+
+class TestFuncionesImagen(unittest.TestCase):
+    
+    
+    def assertNotEmpty(self, obj):
+        self.assertTrue(obj)
+    
+    def test_comparacion_algoritmos(self):
+        self.assertNotEmpty(comparacion_algoritmos())
+    
+    def test_tokenize_equal(self):
+        self.assertEqual(tokenize('Enfado'),['enfad'])
+        
+    def test_tokenize_empty(self):
+        self.assertNotEmpty(tokenize('Enfado'))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
